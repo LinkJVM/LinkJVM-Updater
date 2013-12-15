336295792327f46ef74b3dbdc6390225e9946944
kleiinnn <m@mklein.co.at>
2013-12-15 22:27:12 +0100

    move content of shell script directly into c file

--- linkjvm_updater.c
+++ linkjvm_updater.c
@@ -3,5 +3,11 @@
 
 int main(){
 	printf("LinkJVM Updater v1.0\n(c)2013, Markus Klein\nMIT License");
-	return system("sh update.sh");
+	return system(
+	"cd /tmp;\
+	echo 'Fetching latest LinkJVM package...';\
+	wget http://files.mklein.co.at/LinkJVM/packages/latest.ipk;\
+	echo 'Installing latest LinkJVM package...';\
+	opkg upgrade latest.ipk;\
+	echo 'Done.';");
 }
--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd /tmp
-echo "Fetching latest LinkJVM package..."
-wget http://files.mklein.co.at/LinkJVM/packages/latest.ipk
-echo "Installing latest LinkJVM package..."
-opkg upgrade latest.ipk
-echo "Done."
