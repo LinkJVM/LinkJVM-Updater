e604d4eeaae29c415596590edc32da3929cb4ee3
kleiinnn <m@mklein.co.at>
2013-12-15 22:10:09 +0100

    added some debug outputs

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 cd /tmp
+echo "Fetching latest LinkJVM package..."
 wget http://files.mklein.co.at/LinkJVM/packages/latest.ipk
+echo "Installing latest LinkJVM package..."
 opkg upgrade latest.ipk
+echo "Done."
