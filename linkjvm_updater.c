b798a7f354bef00f10b37974fdce94a53b4455a8
kleiinnn <m@mklein.co.at>
2013-12-15 21:57:18 +0100

    implemented updater

---
+++ linkjvm_updater.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	printf("LinkJVM Updater v1.0\n(c)2013, Markus Klein\nMIT License");
+	return system("update.sh");
+}
---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd /tmp
+wget http://files.mklein.co.at/LinkJVM/packages/latest.ipk
+opkg upgrade latest.ipk
